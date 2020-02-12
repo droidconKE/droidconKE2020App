f7a19f78021c1e44b8153f026948ca6c15cbb29a
Harun Wangereka <harunwangereka@gmail.com>
2020-02-12 11:36:51 +0300

    App flow detup complete. This addresses #11

---
+++ features/about/src/main/java/com/android254/droidconKE2020/about/ui/views/AboutFragment.kt
@@ -0,0 +1,7 @@
+package com.android254.droidconKE2020.about.ui.views
+
+import androidx.fragment.app.Fragment
+import com.android254.droidconKE2020.about.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+}
