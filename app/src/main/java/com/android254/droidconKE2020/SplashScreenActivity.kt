edbd92206bce50bc526b5664492027cd431af3fc
Jama Mohamed <jama3137@gmail.com>
2020-03-22 12:21:57 +0300

    Added SplashScreenActivity

---
+++ app/src/main/java/com/android254/droidconKE2020/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.android254.droidconKE2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
