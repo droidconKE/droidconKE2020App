09f1eac29e37c6ffde12a5de82885566d9da9e9f
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-04-03 14:10:52 +0300

    Remove all changes on the toolbar not pertaining dark theme to ensure PR matched scope

--- app/src/main/java/com/android254/droidconKE2020/HomeActivity.kt
+++ app/src/main/java/com/android254/droidconKE2020/HomeActivity.kt
@@ -6,22 +6,13 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.android254.droidconKE2020.toolbar.CountdownFragment
-import com.android254.droidconKE2020.toolbar.FeedbackFragment
-import com.android254.droidconKE2020.toolbar.TitleFragment
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.content_home.*
 
 class HomeActivity : AppCompatActivity() {
-
-    // ToDo: Move this to viewmodel
-    private val isAuthenticated = MutableLiveData<Boolean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,21 +39,13 @@ class HomeActivity : AppCompatActivity() {
             }
             R.id.action_sign_in -> {
                 signIn()
-                if (isAuthenticated.value == true) {
-                    isAuthenticated.value = false
-                    item.icon = ContextCompat.getDrawable(baseContext, R.drawable.ic_login)
-                } else {
-                    isAuthenticated.value = true
-                    item.icon = ContextCompat.getDrawable(baseContext, R.drawable.ic_profile_pic)
-                }
-
                 true
             }
-//            R.id.action_settings -> true
-//            R.id.action_feedback -> {
-//                feedback()
-//                true
-//            }
+            R.id.action_settings -> true
+            R.id.action_feedback -> {
+                feedback()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -79,7 +62,6 @@ class HomeActivity : AppCompatActivity() {
             when (destination.id) {
                 R.id.aboutFragment, R.id.homeFragment, R.id.feedFragment, R.id.sessionsFragment -> {
                     bottomNavigation.visibility = View.VISIBLE
-                    showToolbarContent(isAuthenticated.value ?: false, destination.id)
                 }
                 else -> bottomNavigation.visibility = View.GONE
             }
@@ -90,41 +72,18 @@ class HomeActivity : AppCompatActivity() {
         findNavController(R.id.nav_host_fragment).navigate(R.id.authDialog)
     }
 
+    private fun feedback() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        navController.navigate(R.id.feedBackFragment)
+    }
+
     private fun toggleDarkTheme() {
         val newTheme = when (AppCompatDelegate.getDefaultNightMode()) {
             AppCompatDelegate.MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_NO
             else -> AppCompatDelegate.MODE_NIGHT_YES
         }
         AppCompatDelegate.setDefaultNightMode(newTheme)
-    }
-
-    private fun showToolbarContent(isAuthenticated: Boolean, destinationId: Int) {
-
-        val content = when (destinationId) {
-            R.id.homeFragment -> if (isAuthenticated) FeedbackFragment() else CountdownFragment()
-            R.id.feedFragment -> TitleFragment().also {
-                val args = Bundle()
-                args.putString("title", getString(R.string.feed))
-                it.arguments = args
-            }
-            R.id.sessionsFragment -> TitleFragment().also {
-                val args = Bundle()
-                args.putString("title", getString(R.string.sessions))
-                it.arguments = args
-            }
-            R.id.aboutFragment -> TitleFragment().also {
-                val args = Bundle()
-                args.putString("title", getString(R.string.about))
-                it.arguments = args
-            }
-            else -> null
-        }
-
-        content?.let {
-            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-            if (supportFragmentManager.fragments.size > 0)
-                ft.replace(toolbarContent.id, content).commit()
-            else ft.add(toolbarContent.id, content).commit()
-        }
     }
 }
