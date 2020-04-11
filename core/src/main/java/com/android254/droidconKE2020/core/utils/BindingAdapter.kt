a18d0be156dc73adffac42a74c003f0a3d6b6d95
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-04-11 15:02:08 +0300

    Speaker details lanscape variation

--- core/src/main/java/com/android254/droidconKE2020/core/utils/BindingAdapter.kt
+++ core/src/main/java/com/android254/droidconKE2020/core/utils/BindingAdapter.kt
@@ -1,13 +1,19 @@
 package com.android254.droidconKE2020.core.utils
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import cn.gavinliu.android.lib.shapedimageview.ShapedImageView
 import coil.api.load
 import coil.transform.RoundedCornersTransformation
 
 @BindingAdapter("imageUrl")
-fun loadImage(imageView : ShapedImageView,imageUrl : String?){
+fun loadImage(imageView: ShapedImageView, imageUrl: String?) {
     imageView.load(imageUrl) {
         transformations(RoundedCornersTransformation(12f))
     }
+}
+
+@BindingAdapter("rectImageUrl")
+fun rectImageUrl(imageView: ShapedImageView, imageUrl: String?) {
+    imageView.load(imageUrl)
 }
