3df3fa905795df0e7bc2a03fb6a51ee02c5e9e3d
Harun Wangereka <harunwangereka@gmail.com>
2020-05-11 16:04:06 +0300

    Removed build files

--- features/speaker/build/generated/source/kapt/debug/com/android254/droidconKE2020/speaker/BR.java
+++
@@ -1,9 +0,0 @@
-package com.android254.droidconKE2020.speaker;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int speakerDetailsModel = 7929856;
-
-  public static final int speakerImg = 7929857;
-}
