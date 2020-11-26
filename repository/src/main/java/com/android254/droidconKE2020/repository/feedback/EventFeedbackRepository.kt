74e7539877b3cc150b14c1a36fda8e295bd19899
Harun Wangereka <harunwangereka@gmail.com>
2020-11-26 16:42:49 +0300

    added submit event repository

---
+++ repository/src/main/java/com/android254/droidconKE2020/repository/feedback/EventFeedbackRepository.kt
@@ -0,0 +1,23 @@
+package com.android254.droidconKE2020.repository.feedback
+
+import com.android254.droidconKE2020.network.ApiService
+import com.android254.droidconKE2020.repository.Data
+
+interface EventFeedbackRepository {
+    suspend fun sendEventFeedback(feedback : String, rating : String) : Data<String>
+}
+
+class EventFeedbackRepositoryImpl(private val apiService: ApiService) : EventFeedbackRepository {
+    override suspend fun sendEventFeedback(feedback: String, rating: String): Data<String> {
+        return try {
+            val response = apiService.eventFeedback.sendEventFeedback(feedback, rating)
+            when{
+                response.isSuccessful -> Data.Success(response.body()!!.message)
+                else -> Data.Error(response.message())
+            }
+        }catch (exception : Exception){
+            Data.Error(exception.message)
+        }
+    }
+
+}
