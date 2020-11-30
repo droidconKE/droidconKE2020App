e1b4d43492fec506fdb89b8970d9674fbc2bd555
Harun Wangereka <harunwangereka@gmail.com>
2020-11-30 22:22:53 +0300

    added session repository

--- repository/src/main/java/com/android254/droidconKE2020/repository/sessions/SessionRepository.kt
+++ repository/src/main/java/com/android254/droidconKE2020/repository/sessions/SessionRepository.kt
@@ -1,4 +1,26 @@
 package com.android254.droidconKE2020.repository.sessions
 
-class SessionRepository {
+import com.android254.droidconKE2020.network.ApiService
+import com.android254.droidconKE2020.network.responses.Sessions
+import com.android254.droidconKE2020.repository.Data
+
+interface SessionRepository {
+
+    suspend fun fetchSessionsSchedule() : Data<Sessions>
+}
+
+class SessionRepositoryImpl(private val apiService: ApiService) : SessionRepository{
+    override suspend fun fetchSessionsSchedule(): Data<Sessions> {
+        val response = apiService.sessionSchedule.fetchSchedule()
+        return try {
+            when{
+                response.isSuccessful -> Data.Success(response.body()!!)
+                else -> Data.Error(response.message())
+            }
+
+        } catch (exception : Exception) {
+            Data.Error(exception.message)
+        }
+    }
+
 }
