f967d3e3eafb13bb3ea1e3740a47bceb31956c08
Harun Wangereka <harunwangereka@gmail.com>
2020-12-03 17:09:23 +0300

    added session feedback endpoint

---
+++ network/src/main/java/com/android254/droidconKE2020/network/SessionFeedbackEndpoints.kt
@@ -0,0 +1,18 @@
+package com.android254.droidconKE2020.network
+
+import com.android254.droidconKE2020.network.responses.Message
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface SessionFeedbackEndpoints {
+
+    @POST("events/droidconke2021-957/feedback/sessions/{session_slug}")
+    fun submitSessionFeedback(
+        @Path("session_slug") sessionSlug : String,
+        @Field("feedback") feedback: String,
+        @Field("rating") rating: Int
+    ) : Response<Message>
+
+}
