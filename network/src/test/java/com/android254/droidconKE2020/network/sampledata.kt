eb8ef52d24fbeaf0b7ca24c618cf517861cf08cf
Michael Bukachi <michaelbukachi@gmail.com>
2020-12-23 18:39:07 +0300

    fix: Correct broken tests

--- network/src/test/java/com/android254/droidconKE2020/network/sampledata.kt
+++ network/src/test/java/com/android254/droidconKE2020/network/sampledata.kt
@@ -20,14 +20,15 @@ val sampleSession =
         "#7F9337",
         "#7F9337",
         "Dependency Injection via frameworks like Dagger were present in most modern Android projects.\n" +
-            "But then Kotlin happened. And since then a bunch of new libraries like Koin or Kodein appeared. Developers are even writing articles how to do DI without any framework.\n" +
-            "Some argue that those don’t even offer real dependency injection. Let’s look at that argument and compare the approaches.\n" +
-            "So, is there something wrong with Dagger & co. in Kotlin? Are they obsolete? What are the alternatives? Let’s dive in.",
+                "But then Kotlin happened. And since then a bunch of new libraries like Koin or Kodein appeared. Developers are even writing articles how to do DI without any framework.\n" +
+                "Some argue that those don’t even offer real dependency injection. Let’s look at that argument and compare the approaches.\n" +
+                "So, is there something wrong with Dagger & co. in Kotlin? Are they obsolete? What are the alternatives? Let’s dive in.",
         "2021-05-06 08:45:00",
         "08:45:00",
         1,
         false,
         0,
+        false,
         emptyList(),
         "Regular Session",
         "",
@@ -37,7 +38,8 @@ val sampleSession =
         "2021-05-06 08:00:00",
         "08:00:00",
         "To Inject or not inject - Dependency injection in a Kotlin world",
-    )
+
+        )
 
 val sampleSpeaker = Speaker(
     "https://sessionize.com/image?f=b8c9f0300f2d7242f78c4df95bf297f4,400,400,1,0,79-31e3-4c9c-88fe-30d51990bf64.08ff2466-3b86-4bb5-9292-b4b493df6e6f.JPG",
